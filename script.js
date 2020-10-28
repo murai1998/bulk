9dca933f7825bd6e9ddd1b2483d57b8c0ded8443
Hanna Murai <annmuray75@gmail.com>
2020-10-27 20:35:10 -0500

    render-buttons

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-let start = document.querySelector("#start");
 let main = document.querySelector("#main");
 main.style.visibility = "hidden";
-start.onclick = () => {
-  document.querySelector("#intro").remove();
-  main.style.visibility = "visible";
-  start.style.visibility = "hidden";
+const render = () => {
+  let start = document.querySelector("#start");
+
+  let back = document.querySelector("#back");
+  let intro = document.querySelector("#intro");
+  let next = document.querySelector("#next");
+
+  start.onclick = () => {
+    let itm = document.querySelector("#intro");
+
+    let cln = itm.cloneNode(true);
+    console.log(cln);
+    document.querySelector("#body").appendChild(cln);
+    document.querySelector("#intro").remove();
+    let main = document.querySelector("#main");
+    main.style.visibility = "visible";
+    let intro = document.querySelector("#intro");
+    intro.style.visibility = "hidden";
+    render();
+  };
+
+  back.onclick = () => {
+    let itm = document.querySelector("#main");
+    let cln = itm.cloneNode(true);
+    console.log(cln);
+    document.querySelector("#body").appendChild(cln);
+    document.querySelector("#main").remove();
+    let intro = document.querySelector("#intro");
+    let main = document.querySelector("#main");
+    intro.style.visibility = "visible";
+    main.style.visibility = "hidden";
+    render();
+  };
 };
+
+render();
